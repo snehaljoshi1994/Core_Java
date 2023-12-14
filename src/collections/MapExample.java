5a80dc74dab54b64d403f7b3ff226baa5a53e07e
Snehal Joshi <snehaljoshi0010@gmail.com>
2023-12-14 13:56:18 +0530

    initial commit

--- src/collections/MapExample.java
+++ src/collections/MapExample.java
@@ -39,6 +39,29 @@ public class MapExample {
 				System.out.println("The value is " + map1.get(key));
 			}
 		 
+		 
+		// find out occurrence of each word in the string using hashmap
+			
+			
+			String str1 = "clean world green world happy world";
+		 
+	        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+	        String[] words = str1.split(" ");
+	        
+	        int count;
+	        for (String word : words) {
+	            
+	        	count = wordCountMap.getOrDefault(word, 0);
+	        	wordCountMap.put(word, count+1);
+	        }
+	        
+	        // ("clean",1) , count = 0
+	        // ("green",1) , count = 0
+	        // ("happy",1) , count = 0
+	        // ("world",3) , count = 2
+	        
+	        System.out.println(wordCountMap);		 
 	}
 
 }
